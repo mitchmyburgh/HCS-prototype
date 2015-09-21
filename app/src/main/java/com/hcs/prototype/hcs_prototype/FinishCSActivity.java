d872716ab34060e9462694d9c12c696c693d4d23
zeezment <mkwsin003@myuct.ac.za>
2015-09-21 12:24:07 +0200

    checkout page for finished casestudy

--- app/src/main/java/com/hcs/prototype/hcs_prototype/FinishCSActivity.java
+++ app/src/main/java/com/hcs/prototype/hcs_prototype/FinishCSActivity.java
@@ -1,9 +1,11 @@
 package com.hcs.prototype.hcs_prototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class FinishCSActivity extends AppCompatActivity {
 
@@ -33,5 +35,11 @@ public class FinishCSActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void buttonOnClick(View v)
+    {
+    // do something when the button is clicked
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
